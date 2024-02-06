bd405cf820f2e37c914c69994eeba588c71734fd
Arshad <121594418+Md-Arshad22@users.noreply.github.com>
2024-02-06 14:25:42 +0530

    Add files via upload

---
+++ mailer.js
@@ -0,0 +1,37 @@
+nodemailer = require('nodemailer');
+
+
+
+    transporter = nodemailer.createTransport({
+      
+        host :'smtp.gmail.com',
+        port: 465,
+        secure:'true',
+        auth: {
+            user: 'arshadad742@gmail.com',
+            pass :'yuxd fphe vzaf obeg'
+        }
+    })
+
+
+//mail object
+mail = {
+    from:'arshadad742@gmail.com',
+    to:'mdabduls2511@gmail.com',
+    subject: 'OTP',
+    text:'Your OTP is signing is value' 
+    
+
+}
+
+// send mail
+transporter.sendMail(mail, (err, data) => {
+    if(err) {
+        console.log(err)
+    }
+    else{
+        console.log(data)
+    }
+})
+
+
